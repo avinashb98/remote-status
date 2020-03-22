450b4bd97ff6f530454d751a5af064a951233694
Avinash Bhardwaj <avinashb97@gmail.com>
2020-03-23 03:47:03 +0530

    send dashboard data

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -2,6 +2,35 @@
 const createError = require('http-errors')
 const User = require('../models/user')
 
+const arrangeUsersByTeam = (users) => {
+  users = users.map(u => (
+    {
+      ...u._doc,
+      isOnline: u.status === 'Online',
+      isAway: u.status === 'Away',
+      isOffline: u.status === 'Offline',
+      test: 'agfghhjksdkhfhasdhjk'
+    }
+  ))
+  const dashboard = []
+  const teams = {}
+  for (const user of users) {
+    if (!teams[user.team]) {
+      teams[user.team] = [user]
+    } else {
+      teams[user.team].push(user)
+    }
+  }
+
+  for (const team in teams) {
+    dashboard.push({
+      team,
+      users: teams[team]
+    })
+  }
+  return dashboard
+}
+
 class DashboardController {
   static async getUsers (req, res, next) {
     const { organisation } = req.params
@@ -24,9 +53,9 @@ class DashboardController {
       })
       return
     }
-    res.status(201).json({
-      message: `All users of ${organisation}`,
-      data: users
+    const teams = arrangeUsersByTeam(users)
+    res.render('index', {
+      dashboard: { teams }
     })
   }
 }
