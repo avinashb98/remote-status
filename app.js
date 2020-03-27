90dcd7a275f27e296c42e086bfc0949eba1453fa
Avinash Bhardwaj <avinashb97@gmail.com>
2020-03-27 12:54:21 +0530

    Add compression

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const compression = require('compression')
 
 require('./config/db')
 
@@ -10,7 +11,7 @@ const userRouter = require('./src/routes/user')
 const dashboardRouter = require('./src/routes/dashboard')
 
 const app = express()
-
+app.use(compression())
 // view engine setup
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'hbs')
